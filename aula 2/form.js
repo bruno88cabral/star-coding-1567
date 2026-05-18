5028f8b8b4f8033e216e8ef7d394aa12e32ca3eb
bruno cabral <bruno88cabral@gmail.com>
2026-05-18 10:40:19 -0300

    adiciona refatoracao aula 2

--- aula 2/form.js
+++ aula 2/form.js
@@ -2,42 +2,41 @@ const form = document.querySelector("form");
 let erroNome = document.querySelector("#erro-nome");
 let erroIdade = document.querySelector("#erro-idade");
 let erroEmail = document.querySelector("#erro-email");
+let formularioValido = true;
+
+const validarCampo = (mensagem, campo, elementoError) => {
+  elementoError.textContent = mensagem;
+
+  document.getElementById(campo).classList.add("field-error");
+
+  formularioValido = false;
+};
+
+const limparValidacao = (campo, elementoError, value) => {
+  if (value) {
+    elementoError.textContent = "";
+    document.getElementById(campo).classList.remove("field-error");
+  }
+};
 
 // Evento que fica escutando o keyup do campo nome
 document.querySelector("#nome").addEventListener("keyup", (event) => {
-  const value = event.target.value;
-  if (value) {
-    erroNome.textContent = "";
-    document.getElementById("nome").style.borderColor = "#ccc";
-    document.getElementById("nome").style.borderWidth = "1px";
-  }
+  limparValidacao("nome", erroNome, event.target.value);
 });
 
 // Evento que fica escutando o keyup do campo idade
 document.querySelector("#idade").addEventListener("keyup", (event) => {
-  const value = event.target.value;
-  if (value) {
-    erroIdade.textContent = "";
-    document.getElementById("idade").style.borderColor = "#ccc";
-    document.getElementById("idade").style.borderWidth = "1px";
-  }
+  limparValidacao("idade", erroIdade, event.target.value);
 });
 
 // Evento que fica escutando o keyup do campo email
 document.querySelector("#email").addEventListener("keyup", (event) => {
-  const value = event.target.value;
-  if (value) {
-    erroEmail.textContent = "";
-    document.getElementById("email").style.borderColor = "#ccc";
-    document.getElementById("email").style.borderWidth = "1px";
-  }
+  limparValidacao("email", erroEmail, event.target.value);
 });
 
 // Evento que fica escutando a submissão do formulário
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  let formularioValido = true;
 
   // Limpando estado de erro
   erroNome.textContent = "";
@@ -48,34 +47,22 @@ form.addEventListener("submit", (event) => {
   //Validação do campo Nome
   const nome = document.querySelector("#nome").value;
   if (!nome) {
-    erroNome.textContent = "Campo obrigatório";
-    formularioValido = false;
-    document.getElementById("nome").style.borderColor = "red";
-    document.getElementById("nome").style.borderWidth = "2px";
+    validarCampo("Campo obrigatório", "nome", erroNome);
   }
 
   //Validação do campo idade
   const idade = document.querySelector("#idade").value;
   if (!idade) {
-    erroIdade.textContent = "Campo obrigatório";
-    formularioValido = false;
-    document.getElementById("idade").style.borderColor = "red";
-    document.getElementById("idade").style.borderWidth = "2px";
+    validarCampo("Campo obrigatório", "idade", erroIdade);
   }
   if (idade && idade < 18) {
-    erroIdade.textContent = "Deve ser maior que 18";
-    formularioValido = false;
-    document.getElementById("idade").style.borderColor = "red";
-    document.getElementById("idade").style.borderWidth = "2px";
+    validarCampo("Deve ser maior que 18", "idade", erroIdade);
   }
 
   //Validação do campo email
   const email = document.querySelector("#email").value;
   if (!email) {
-    erroEmail.textContent = "Campo obrigatório";
-    formularioValido = false;
-    document.getElementById("email").style.borderColor = "red";
-    document.getElementById("email").style.borderWidth = "2px";
+    validarCampo("Campo obrigatório", "email", erroEmail);
   }
 
   //Resultado da validação
